4b3b5ad992aa7d15e605ec3c9e6442e826f61992
Shawn Coots <shawn@viastudio.com>
2013-03-25 16:21:05 +0000

    - comment fix

--- comments.php
+++ comments.php
@@ -26,7 +26,7 @@
 				 * define via_comment() and that will be used instead.
 				 * See via_comment() in inc/template-tags.php for more.
 				 */
-				wp_list_comments( array( 'callback' => 'chanceschool_comment' ) );
+				wp_list_comments( array( 'callback' => 'via_comment' ) );
 			?>
 		</ol><!-- .commentlist -->
 
