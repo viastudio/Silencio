51bd50303a8c7108fda7c42632c2ed14cadc1fd7
Shawn Coots <shawn@viastudio.com>
2013-05-01 13:48:07 +0000

    - o-embed in widgets added

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ if ( ! function_exists( 'via_setup' ) ):
 function via_setup() {
 
 	require_once( dirname( __FILE__ ) . '/res/functions/admin.php' );
-	
+
 	require( get_template_directory() . '/res/functions/template-tags.php' );
 	require( get_template_directory() . '/res/functions/shortcodes.php' );
 	require( get_template_directory() . '/res/functions/widgets.php' );
@@ -31,6 +31,10 @@ if ( function_exists( 'add_image_size' ) ) {
 	add_image_size( 'slide-thumb', 1170, 400, true ); //(cropped)
 }
 
+// Add oEmbed to Widgets
+add_filter( 'widget_text', array( $wp_embed, 'run_shortcode' ), 8 );
+add_filter( 'widget_text', array( $wp_embed, 'autoembed'), 8 );
+
 /**
  * Register widgetized area and update sidebar with default widgets
  */
@@ -44,7 +48,7 @@ function via_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	));
-	
+
 	register_sidebar( array(
 		'name' => __( 'Page Sidebar', 'via' ),
 		'id' => 'page-sidebar',
@@ -53,7 +57,7 @@ function via_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	));
-	
+
 	register_sidebar( array(
 		'name' => __( 'Post Sidebar', 'via' ),
 		'id' => 'post-sidebar',
@@ -62,7 +66,7 @@ function via_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	));
-	
+
 	register_sidebar( array(
 		'name' => __( 'Footer Sidebar', 'via' ),
 		'id' => 'footer-sidebar',
@@ -71,7 +75,7 @@ function via_widgets_init() {
 		'before_title' => '<h3 class="widget-title">',
 		'after_title' => '</h3>',
 	));
-	
+
 }
 
 add_action( 'widgets_init', 'via_widgets_init' );
@@ -104,15 +108,15 @@ function via_scripts() {
         $global = 'build/global.min.js';
         $style = 'build/global.min.css';
     }
-    
+
     wp_enqueue_script('global', get_template_directory_uri() . '/res/' . $global, array('jquery'), filemtime(get_stylesheet_directory() . '/res/' . $global), true);
 
-	// Register the style like this for a theme:  
+	// Register the style like this for a theme:
 	wp_register_style( 'via-style', get_template_directory_uri() . '/res/' . $style, array(), filemtime(get_stylesheet_directory() . '/res/'. $style), 'all' );
 
 	// enqueing:
 	wp_enqueue_style( 'via-style' );
-	
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -261,7 +265,7 @@ function via_build_directions_url($street, $city, $state, $zip) {
 
 	// Serve up an Apple Maps link if on an iOS 6 device, otherwise serve up a Google Maps link.
 	if ($iOS_version >= 6) {
-		return "https://maps.apple.com/maps?q=" . $address; 
+		return "https://maps.apple.com/maps?q=" . $address;
 	}
 	else {
 		return "https://maps.google.com/maps?q=" . $address;
