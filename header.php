b78e4a8f5a8b9f74195c03d58a43a3fd1f9e2142
Joe Flynn <joe@viastudio.com>
2014-04-04 11:18:42 -0400

    Off-canvas menu

--- header.php
+++ header.php
@@ -39,18 +39,27 @@
 </head>
 
 <body <?php body_class(); ?>>
+    <div class="body-wrap">
+    	<div id="page" class="hfeed site">
+    		<header id="masthead" class="site-header" role="banner">
+                <nav class="menu-nav">
+                    <a class="menu-button" href="#"><i class="fa fa-bars"></i></a>
+                </nav>
 
-	<div id="page" class="hfeed site">
+    			<div class="site-branding">
+    				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+    				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    			</div><!-- .site-branding -->
 
-		<header id="masthead" class="site-header" role="banner">
-			<div class="site-branding">
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div><!-- .site-branding -->
+                <div class="nav-container">
+        			<nav id="site-navigation" class="main-navigation" role="navigation">
+        				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+        			</nav><!-- #site-navigation -->
+                    <nav id="ancillary-navigation" class="ancillary-navigation" role="navigation">
+                        <?php wp_nav_menu( array( 'theme_location' => 'ancillary' ) ); ?>
+                    </nav>
+                </div>
 
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead .site-header -->
+    		</header><!-- #masthead .site-header -->
 
-		<div id="content" class="site-content">
+    		<div id="content" class="site-content">
