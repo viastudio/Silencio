288e223ac9345ae3ad04abfed4b7ad11961bc4f3
Shawn Coots <shawncoots@gmail.com>
2014-07-24 21:59:03 -0400

    Structure setup.

--- sidebar-post.php
+++
@@ -1,3 +0,0 @@
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('post-sidebar') ) { } ?>
-		</div><!-- #secondary .widget-area -->
