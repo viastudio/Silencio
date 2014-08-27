a7635d67042136727c54f8f1f1d5236a0095fd02
Joe Flynn <joe@viastudio.com>
2014-08-27 16:27:25 -0400

    reduce DB queries in footer theme options

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <?php
+$options = get_theme_mods();
+
 $directions = silencio_build_directions_url(
-    get_theme_mod('theme_option_street_txt_input'),
-    get_theme_mod('theme_option_city_txt_input'),
-    get_theme_mod('theme_option_state_txt_input'),
-    get_theme_mod('theme_option_zip_txt_input')
+    $options['theme_option_street_txt_input'],
+    $options['theme_option_city_txt_input'],
+    $options['theme_option_state_txt_input'],
+    $options['theme_option_zip_txt_input']
 );
 ?>
                 </div><!-- .container -->
@@ -25,35 +27,29 @@ $directions = silencio_build_directions_url(
                         <ul class="address" itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address">
                             <li>&#169; <?php echo date('Y');?>  <span itemprop="name"><?php bloginfo('name'); ?></span></li>
 <?php
-if ((
-    $street = get_theme_mod('theme_option_street_txt_input')) != '') {
+if ($options['theme_option_street_txt_input'] != '') {
 ?>
-                            <li><span itemprop="street-address"><?php echo $street; ?></span></li>
+                            <li><span itemprop="street-address"><?php echo $options['theme_option_street_txt_input']; ?></span></li>
 <?php
-} if ((
-    $city = get_theme_mod('theme_option_city_txt_input')) != '') {
+} if ($options['theme_option_city_txt_input'] != '') {
 ?>
-                            <li><span itemprop="locality"><?php echo $city; ?></span>,
+                            <li><span itemprop="locality"><?php echo $options['theme_option_city_txt_input']; ?></span>,
 <?php
-} if ((
-    $state = get_theme_mod('theme_option_state_txt_input')) != '') {
+} if ($options['theme_option_state_txt_input'] != '') {
 ?>
-                                <span itemprop="region"><?php echo $state; ?></span>
+                                <span itemprop="region"><?php echo $options['theme_option_state_txt_input']; ?></span>
 <?php
-} if ((
-    $zip = get_theme_mod('theme_option_zip_txt_input')) != '') {
+} if ($options['theme_option_zip_txt_input'] != '') {
 ?>
-                                <span itemprop="postal-code"><?php echo $zip; ?></span></li>
+                                <span itemprop="postal-code"><?php echo $options['theme_option_zip_txt_input']; ?></span></li>
 <?php
-} if ((
-    $phone = get_theme_mod('theme_option_phone_txt_input')) != '') {
+} if ($options['theme_option_phone_txt_input'] != '') {
 ?>
-                            <li><span itemprop="tel"><?php echo $phone; ?></span></li>
+                            <li><span itemprop="tel"><?php echo $options['theme_option_phone_txt_input']; ?></span></li>
 <?php
-} if ((
-    $email = get_theme_mod('theme_option_email_txt_input')) != '') {
+} if ($options['theme_option_email_txt_input'] != '') {
 ?>
-                            <li><span itemprop="email"><a href="<?php echo $email; ?>">Email</a></span></li>
+                            <li><span itemprop="email"><a href="<?php echo $options['theme_option_email_txt_input']; ?>">Email</a></span></li>
 <?php
 }
 ?>
@@ -65,25 +61,21 @@ if ((
 
                         <ul class="social-media">
 <?php
-if ((
-    $facebook = get_theme_mod('theme_option_facebook_txt_input')) != '') {
+if ($options['theme_option_facebook_txt_input'] != '') {
 ?>
-                            <li><a href="<?php echo $facebook; ?>"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="<?php echo $options['theme_option_facebook_txt_input']; ?>"><i class="fa fa-facebook"></i></a></li>
 <?php
-} if ((
-    $twitter = get_theme_mod('theme_option_twitter_txt_input')) != '') {
+} if ($options['theme_option_twitter_txt_input'] != '') {
 ?>
-                            <li><a href="<?php echo $twitter; ?>"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="<?php echo $options['theme_option_twitter_txt_input']; ?>"><i class="fa fa-twitter"></i></a></li>
 <?php
-} if ((
-    $youtube = get_theme_mod('theme_option_youtube_txt_input')) != '') {
+} if ($options['theme_option_youtube_txt_input'] != '') {
 ?>
-                            <li><a href="<?php echo $youtube; ?>"><i class="fa fa-youtube-play"></i></a></li>
+                            <li><a href="<?php echo $options['theme_option_youtube_txt_input']; ?>"><i class="fa fa-youtube-play"></i></a></li>
 <?php
-} if ((
-    $googleplus = get_theme_mod('theme_option_googleplus_txt_input')) != '') {
+} if ($options['theme_option_googleplus_txt_input'] != '') {
 ?>
-                            <li><a href="<?php echo $googleplus; ?>"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="<?php echo $options['theme_option_googleplus_txt_input']; ?>"><i class="fa fa-google-plus"></i></a></li>
 <?php
 }
 ?>
