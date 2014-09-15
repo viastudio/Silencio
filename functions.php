ebb59d8ea9c21cd9674c4f781208c5d787a1196c
Shawn Coots <shawn@viastudio.com>
2014-09-15 15:45:19 -0400

    Removed sidebars file, added to functions.php.

--- functions.php
+++ functions.php
@@ -83,6 +83,56 @@ function silencio_scripts() {
 
 add_action('wp_enqueue_scripts', 'silencio_scripts');
 
+/**
+ * Add oEmbed support for widgets
+ */
+add_filter('widget_text', array($wp_embed, 'run_shortcode'), 8);
+add_filter('widget_text', array($wp_embed, 'autoembed'), 8);
+
+/**
+ * Register widgetized area and update sidebar with default widgets
+ */
+function silencio_widgets_init() {
+
+    register_sidebar(array(
+        'name' => __('Home Sidebar', 'silencio'),
+        'id' => 'home-sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => __('Page Sidebar', 'silencio'),
+        'id' => 'page-sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+    ));
+
+    register_sidebar(array(
+        'name' => __('Post Sidebar', 'silencio'),
+        'id' => 'post-sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>'
+    ));
+
+    register_sidebar( array(
+        'name' => __( 'Footer Sidebar', 'silencio' ),
+        'id' => 'footer-sidebar',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget' => "</aside>",
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
+}
+
+add_action('widgets_init', 'silencio_widgets_init');
+
 
 /**
  * Fix Uncaught Reference Error from Gravity Forms - Puts js call in the footer, where it should be.
