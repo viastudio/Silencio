1a186335926eb085a930242a93c24a96ba1096bb
Natalie Miller <natalie@viastudio.com>
2015-01-21 10:03:09 -0500

    changed widget function on child pages widget

--- res/functions/widgets/SilencioChildrenPages.php
+++ res/functions/widgets/SilencioChildrenPages.php
@@ -18,33 +18,34 @@ class SilencioChildrenPages extends WP_Widget {
 * @param array $args     Widget arguments.
 * @param array $instance Saved values from database.
 */
-public function widget($args, $instance) {
-    $post = $GLOBALS['post'];
-    $top_ancestor = get_page(array_reverse(get_post_ancestors($post->ID))[0]);
-    $title = get_the_title($top_ancestor->ID);
-    extract($args);
-    if ($post->post_parent) {
-        $children = wp_list_pages("title_li=&child_of=" . $top_ancestor->ID . "&echo=0");
-    } else {
-        $children = wp_list_pages("title_li=&child_of=" . $post->ID . "&echo=0");
-    }
-    $title = apply_filters('widget_title', $title);
-    if ($children) {
-        echo $before_widget;
-        if (!empty($title)) {
-            echo $before_title . '<a href="' . get_permalink($top_ancestor->ID) . '">' . $title . '</a>' . $after_title;
+    public function widget($args, $instance) {
+        $post = $GLOBALS['post'];
+        $top_ancestor = get_page(array_reverse(get_post_ancestors($post->ID))[0]);
+        $title = get_the_title($top_ancestor->ID);
+        extract($args);
+        if ($post->post_parent) {
+            $children = wp_list_pages("title_li=&child_of=" . $top_ancestor->ID . "&echo=0");
+        } else {
+            $children = wp_list_pages("title_li=&child_of=" . $post->ID . "&echo=0");
         }
-        ?>
+        $title = apply_filters('widget_title', $title);
+        if ($children) {
+            echo $before_widget;
+            if (!empty($title)) {
+                echo $before_title . '<a href="' . get_permalink($top_ancestor->ID) . '">' . $title . '</a>' . $after_title;
+            }
+?>
         <ul class="nav-parent">
-            <?php
-            echo $children;
-            ?>
+<?php
+        echo $children;
+?>
         </ul>
 
-        <?php
+<?php
+        }
+            echo $after_widget;
     }
-    echo $after_widget;
-}
+
 /**
 * Back-end widget form.
 *
@@ -54,7 +55,7 @@ public function widget($args, $instance) {
 */
 public function form($instance) {
     /* Set up some default widget settings. */
-    ?>
+?>
 
     <p>This widget lists all child pages of the current parent. It displays nothing if there aren't any children.</p>
 
