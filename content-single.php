bd5be8e781965e419155505a55e00410af90a871
= <nick@viastudio.com>
2015-02-25 11:05:32 -0500

    Added featured images to page and single templates

--- content-single.php
+++ content-single.php
@@ -1,4 +1,10 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+if (has_post_thumbnail()) {
+    the_post_thumbnail('header-thumb');
+}
+?>
+
     <header class="entry-header">
         <h1 class="entry-title"><?php the_title(); ?></h1>
 
