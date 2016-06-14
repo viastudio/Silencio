29e501943339075a398f1141b0c83417b467d848
Natalie Miller <natalie@viastudio.com>
2016-06-14 09:44:30 -0400

    Clean up function so that it no longer throws mergeatron errors

--- res/functions/template-tags.php
+++ res/functions/template-tags.php
@@ -256,35 +256,30 @@ if (!function_exists('silencio_footer_meta')) {
  * Returns true if a blog has more than 1 category.
  */
 function silencio_categorized_blog() {
-    if (
-        false === (
-            $all_the_cool_cats = get_transient('all_the_cool_cats'))) {
+    $post_categories = get_transient('post_categories');
+    if (! $post_categories) {
         // Create an array of all the categories that are attached to posts.
-        $all_the_cool_cats = get_categories(array(
+        $post_categories = get_categories(array(
             'hide_empty' => 1
         ));
 
         // Count the number of categories that are attached to the posts.
-        $all_the_cool_cats = count($all_the_cool_cats);
+        $post_categories = count($post_categories);
 
-        set_transient('all_the_cool_cats', $all_the_cool_cats);
+        set_transient('post_categories', $post_categories);
     }
 
-    if ('1' != $all_the_cool_cats) {
-        // This blog has more than 1 category so silencio_categorized_blog should return true.
+    if ($post_categories > 1) {
         return true;
-    } else {
-        // This blog has only 1 category so silencio_categorized_blog should return false.
-        return false;
     }
+    return false;
 }
 
 /**
  * Flush out the transients used in silencio_categorized_blog.
  */
 function silencio_category_transient_flusher() {
-    // Like, beat it. Dig?
-    delete_transient('all_the_cool_cats');
+    delete_transient('post_categories');
 }
 add_action('edit_category', 'silencio_category_transient_flusher');
 add_action('save_post', 'silencio_category_transient_flusher');
