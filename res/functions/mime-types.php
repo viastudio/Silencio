42091fbb70d25a5c2ed5e9f7a11c55d64f36308c
Mark Biek <mark@viastudio.com>
2016-10-31 12:08:43 -0400

    Typo fix

--- res/functions/mime-types.php
+++ res/functions/mime-types.php
@@ -2,7 +2,7 @@
 
 add_filter('upload_mimes', 'custom_mime_types');
 
-function custom_mime_types($exisiting_mimes = array()) {
+function custom_mime_types($existing_mimes = array()) {
     //Add, update, or remove any mime-types you'd like to allow in the media library
 
     //For example, the following line allows PDF uploads
