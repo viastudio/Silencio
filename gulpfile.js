4610a121d1032909f8d8fbce1d7126ebf7483e91
Mark Biek <support@janustech.net>
2017-01-31 13:13:50 -0500

    Added gulpfile
    * `gulp` builds all CSS/LESS/JS
      * (Can build individually with `gulp scripts` or `gulp styles`
    * `gulp watch` watches for changes
    * `gulp clean` deletes contents of **res/build**

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+const uglify = require('gulp-uglify');
+const run = require('run-sequence');
+const merge = require('merge-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const del = require('del');
+const rename = require('gulp-rename');
+const gutil = require('gulp-util');
+
+const paths = {
+    scripts: [
+        'res/components/jquery/dist/jquery.js',
+        'res/components/bootstrap/dist/js/bootstrap.js',
+        'res/components/fitvids/jquery.fitvids.js',
+        'res/components/picturefill/dist/picturefill.js',
+        'res/js/global.js'
+    ],
+    css: ['res/components/**/*.css', 'res/css/**/*css'],
+    less: ['res/less/**/*.less'],
+    out: 'res/build/'
+};
+
+gulp.task('scripts', () => {
+    var respond = gulp.src('res/components/respond/dest/respond.src.js')
+        .pipe(rename('respond.min.js'))
+        .pipe(gulp.dest(paths.out));
+
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify().on('error', gutil.log))
+        .pipe(concat('global.min.js'))
+        .pipe(sourcemaps.write({
+            includeContent: false
+        }))
+        .pipe(gulp.dest(paths.out));
+});
+
+gulp.task('styles', () => {
+    var lessStream = gulp.src(paths.less)
+        .pipe(less());
+
+    var cssStream = gulp.src(paths.css);
+
+    var merged = merge(lessStream, cssStream)
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(cssmin())
+        .pipe(concat('global.min.css'))
+        .pipe(sourcemaps.write({
+            includeContent: false
+        }))
+        .pipe(gulp.dest(paths.out));
+
+    return merged;
+});
+
+gulp.task('watch', () => {
+    gulp.watch('res/js/**/*js', ['scripts']);
+    gulp.watch('res/css/**/*.css', ['styles']);
+    gulp.watch('res/less/**/*.less', ['styles']);
+});
+
+gulp.task('clean', () => {
+    return del([
+        'res/build/*'
+    ]);
+});
+
+gulp.task('default', ['styles', 'scripts']);
