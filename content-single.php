ea6ca02b38fa4430a1739e443f2bd23b5ea86c36
Natalie Miller <nataliemiller03@gmail.com>
2017-06-08 11:10:29 -0400

    move containers in archives and blog post templates

--- content-single.php
+++ content-single.php
@@ -5,22 +5,24 @@ if (has_post_thumbnail()) {
 }
 ?>
     <header class="entry-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <div class="container">
+            <h1 class="entry-title"><?php the_title(); ?></h1>
+                <div class="entry-meta">
 
-        <div class="entry-meta">
 <?php
 silencio_posted_on();
 ?>
+                </div>
         </div><!-- .entry-meta -->
     </header><!-- .entry-header -->
 
-    <div class="entry-content">
+    <div class="entry-content container">
 <?php
 the_content();
 ?>
     </div><!-- .entry-content -->
 
-    <footer class="footer-meta">
+    <footer class="footer-meta container">
 <?php
 silencio_footer_meta();
 ?>
