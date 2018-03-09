cd1a97de19defb43019b072012f98ef8c3f82336
Joel Jacob <joel@viastudio.com>
2018-03-09 15:06:02 -0500

    Style archives

--- content.php
+++ content.php
@@ -1,55 +1,41 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('archive-article'); ?>>
-    <header class="entry-header">
-        <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-        <div class="entry-meta">
-<?php
-    silencio_posted_on();
-?>
-        </div><!-- .entry-meta -->
-    </header><!-- .entry-header -->
-
-    <div class="row">
-
+<article id="post-<?php the_ID(); ?>" <?php post_class('archive-article tile is-parent is-6'); ?>>
+    <div class="card">
 <?php
 if (has_post_thumbnail()) {
 ?>
-        <div class="col-md-9">
-            <div class="entry-summary">
-<?php
-    the_excerpt();
-?>
-            </div><!-- .entry-summary -->
-
-            <footer class="footer-meta">
-<?php
-    silencio_footer_meta();
-?>
-            </footer><!-- .footer-meta -->
-        </div><!-- .col-md-9-->
-        <div class="col-md-3">
+        <div class="card-image">
+            <a href="<?php the_permalink(); ?>" rel="bookmark">
+                <figure class="image is-2by1">
 <?php
     the_post_thumbnail('blog-thumb');
 ?>
-        </div><!-- .col-md-3-->
+                </figure>
+            </a>
+        </div><!-- .card-image -->
 <?php
-} else {
+}
 ?>
-        <div class="col-md-12">
-            <div class="entry-summary">
+        <div class="card-content content">
+            <header class="entry-header">
+                <h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+                <div class="entry-meta">
+<?php
+    silencio_posted_on();
+?>
+                </div><!-- .entry-meta -->
+            </header><!-- .entry-header -->
+            <div class="entry-summary content">
 <?php
     the_excerpt();
 ?>
             </div><!-- .entry-summary -->
+
 
             <footer class="footer-meta">
 <?php
     silencio_footer_meta();
 ?>
             </footer><!-- .footer-meta -->
-        </div><!-- .col-md-12-->
-<?php
-}
-?>
-
-    </div><!--.row -->
+        </div><!-- .card -->
+    </div><!--.card-content -->
 </article><!-- #post-## -->
